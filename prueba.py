952594abb5e11d1d2e4556698a1b7d3923e1e5e9
CArranz314 <132569169+CArranz314@users.noreply.github.com>
2023-07-26 08:19:36 +0200

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,8 @@
+variable_1=variable1
+variable_2=variable2
+list=[]
+for i in range(variable_1*variable_2):
+  list.append(variable_1-variable_2)
+
+result=[sum(list),variable_1+variable_2]
+print(result)
